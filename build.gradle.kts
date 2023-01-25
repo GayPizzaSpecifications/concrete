426020f1f983cf78cfed7f2d997a57386abaa2ed
Alex Zenla <alex@fork.run>
2023-01-24 20:26:25 -0800

    GitLab Publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,12 +69,30 @@ publishing {
       githubPackagesToken = project.findProperty("github.token") as String?
     }
 
+    var gitlabPackagesToken = System.getenv("GITLAB_TOKEN")
+    if (gitlabPackagesToken == null) {
+      gitlabPackagesToken = project.findProperty("gitlab.com.accessToken") as String?
+    }
+
     maven {
       name = "GitHubPackages"
       url = uri("https://maven.pkg.github.com/gaypizzaspecifications/concrete")
       credentials {
         username = project.findProperty("github.username") as String? ?: "gaypizzaspecifications"
         password = githubPackagesToken
+      }
+    }
+
+    maven {
+      name = "GitLab"
+      url = uri("https://gitlab.com/api/v4/projects/42873094/packages/maven")
+      credentials(HttpHeaderCredentials::class.java) {
+        name = "Private-Token"
+        value = gitlabPackagesToken
+      }
+
+      authentication {
+        create<HttpHeaderAuthentication>("header")
       }
     }
   }
