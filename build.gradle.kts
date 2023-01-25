a06be056456669dbf673cf051d2e5a6c98ff0601
Alex Zenla <alex@fork.run>
2023-01-24 20:11:07 -0800

    Attempt to fix GitHub packages token.

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,12 +64,17 @@ publishing {
   repositories {
     mavenLocal()
 
+    var githubPackagesToken = System.getenv("GITHUB_TOKEN")
+    if (githubPackagesToken == null) {
+      githubPackagesToken = project.findProperty("github.token") as String?
+    }
+
     maven {
       name = "GitHubPackages"
       url = uri("https://maven.pkg.github.com/gaypizzaspecifications/concrete")
       credentials {
-        username = project.findProperty("github.username") as String? ?: "unknown"
-        password = project.findProperty("github.token") as String? ?: System.getenv("GITHUB_TOKEN")
+        username = project.findProperty("github.username") as String? ?: "gaypizzaspecifications"
+        password = githubPackagesToken
       }
     }
   }
